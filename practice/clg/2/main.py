fae3348032255b4dc84af2c3838a9487ded38cdf
Harsh Panchigar <harshpanchigar@gmail.com>
2026-08-25 19:38:25 +0530

    exam

--- practice/clg/2/main.py
+++ practice/clg/2/main.py
@@ -1,1 +1,15 @@
-print('Hello world')
+import numpy as np
+
+matrix = np.array([
+    [11, 12, 13],
+    [21, 22, 23],
+    [31, 32, 33]
+])
+
+arr = matrix.ravel()
+
+print("Original Matrix:")
+print(matrix)
+
+print("\nOne-Dimensional Array:")
+print(arr)
